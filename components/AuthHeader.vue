2f69e8e4780478757876d28aaf819bf85c331c06
nakamura ibuki <trees.animal47@gmail.com>
2024-08-19 12:23:02 +0900

    push aseets and components

---
+++ components/AuthHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="header flex">
+        <div class="left">
+            <img class="logo" src="../assets/logo.png">
+        </div>
+        <div class="right flex">
+            <p @click="$router.push('/register')">新規登録</p>
+            <p @click="$router.push('/login')">ログイン</p>
+        </div>
+    </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.right{
+    align-items: center;
+}
+.right p{
+    margin-right: 20px;
+    cursor: pointer;
+}
+.header{
+    margin: 20px;
+}
+.logo{
+    width: 150px;
+    cursor: pointer;
+}
+.flex{
+    display: flex;
+    justify-content: space-between;
+}
+</style>
